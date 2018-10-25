9d2466f858470d8d7ff765773a3d936765903baf
focusguy16 <eagle15e2000@yahoo.com>
2018-10-26 01:12:55 +0300

    Building negative test - wrong login details test

--- dpreview-automation/src/main/java/com/dpreview/auto/tests/DpreviewLoginWrongDetailsTest.java
+++ dpreview-automation/src/main/java/com/dpreview/auto/tests/DpreviewLoginWrongDetailsTest.java
@@ -26,5 +26,44 @@ public class DpreviewLoginWrongDetailsTest extends BaseTest {
 		driver.findElement(By.cssSelector(".userTools > a:nth-child(1)")).click();
 		
 	}
+	
+	// ------------------------- Testing the login process with wrong details --------------------------------------------------------------
+	
+		@Test(description = "Typing wrong login details", priority = 1)
+		public void loginDetails1() throws Exception {
+
+			String inputEmail = "eagle15@yahoo.com";
+			String inputPassword = "AlphaBlondie";
+			
+			DpreviewLoginPage dpreviewLoginPage = new DpreviewLoginPage(driver);
+			dpreviewLoginPage.writeToEmailInput(inputEmail);
+			
+			Thread.sleep(5000);
+			
+			dpreviewLoginPage.writeToPasswordInput(inputPassword);
+			
+			Thread.sleep(5000);
+			
+			dpreviewLoginPage.keepMeSignedInDetails();
+			
+			Thread.sleep(10000);
+			
+			dpreviewLoginPage.closingSignedInDetailsPopup();
+			
+			dpreviewLoginPage.keepMeSignedInCheckbox();
+			
+			Thread.sleep(5000);
+			
+			dpreviewLoginPage.clickSigninButton(); 
+			
+			if (dpreviewLoginPage != null) {
+				
+				System.out.println("The login details are correct.");
+			}
+			else {
+				
+				System.out.println("The login details are incorrect.");
+			}
+		}
 
 }
